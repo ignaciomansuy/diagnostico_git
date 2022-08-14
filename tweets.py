92bc3a189739cdf441af39a30d801287f36ca8c3
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-08-14 13:22:30 -0400

    feat: top10 users donde

--- tweets.py
+++ tweets.py
@@ -3,7 +3,11 @@ from collections import defaultdict
 def top10_retweeted(jsons_list):
     print(sorted(jsons_list, key=lambda json_: json_["retweetCount"])[:10])
 
-
-
+def top10_most_comments(jsons_list):
+    dict_count = defaultdict(int)
+    map(lambda json_: dict_count["user"]["id"] += 1, jsons_list)
+    top10 = list(dict_count.items()).sort(key=lambda item: item[1])[:10]
+    top10_ids = [item[0] for item in top10]
+    print(top10_ids)
 
 
